fe6bbb847a21cfef5e6fbdbb607304b825806311
Iván López Muñoz <ivanlopem97@gmail.com>
2019-09-30 14:32:35 +0200

    modelo inicial

---
+++ cliente/modelo.js
@@ -0,0 +1,31 @@
+
+function Juego(){
+	this.partidas=[];
+	this.usuarios=[];
+
+	this.crearPartida = function(nombre, nick){
+		var idp= nombre+nick;
+		if(!this.partidas[idp]){
+		this.partidas[idp] = new Partida(nombre, idp);
+	}
+	}
+
+	this.agregarUsuario= function(nombre){
+		if (this.usuarios[nombre]==null){
+			this.usuarios[nombre]= new Usuario(nombre);
+		}
+
+	}
+}
+
+function Partida(nombre, idp){
+	
+	this,nombre = nombre;
+	this.jugadores = [];
+	this.idp = idp;
+}
+
+function Usuario(nick){
+	this.nick = nick;
+	this.id = undefined;
+}
