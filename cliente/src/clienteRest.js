72bd1bf5b845583145d4b1beddd5536aa8948103
Iván López Muñoz <ivanlopem97@gmail.com>
2019-10-09 10:17:53 +0200

    Realizadas las pruebas en el servidor

---
+++ cliente/src/clienteRest.js
@@ -0,0 +1,32 @@
+function ClienteRest(){
+
+	this.agregarUsuario=function(nick){
+		$.getJSON("/agregarUsuario/"+nick,function(data){    
+    		console.log(data);
+    		//mostrarUsuario(data);
+		});
+	}
+	this.crearPartida=function(nombrePartida,nick){
+		$.getJSON("/crearPartida/"+nombrePartida+"/"+nick,function(data){    
+    		console.log(data);
+    		//mostrarPartida(data);
+		});
+	}
+	this.unirAPartida=function(nombrePartida,nick){
+		$.getJSON("/unirAPartida/"+nombrePartida+"/"+nick,function(data){    
+    		console.log(data);
+    		//mostrarPartida(data);
+		});
+	}
+	this.obtenerPartidas=function(){
+		$.getJSON("/obtenerPartidas",function(data){    
+    		console.log(data);
+    		//mostrarPartidas(data);
+		});
+	}
+	this.obtenerJugadores=function(nombrePartida){
+		$.getJSON("/obtenerJugadores/"+nombrePartida,function(data){
+			console.log(data);
+		})
+	}
+}
---
+++ servidor/modelo.js
@@ -0,0 +1,106 @@
+
+function Juego(){
+	this.partidas={};
+	this.usuarios={};
+
+	this.crearPartida=function(nombre,nick,callback){
+		var idp=nombre+nick;
+		var partida;
+		if (!this.partidas[idp]){
+			partida=new Partida(nombre,idp);
+			partida.agregarJugador(this.usuarios[nick]);
+			//partida.jugadores[nick]=this.usuarios[nick];
+			this.partidas[idp]=partida;
+		}
+		else{
+			partida=this.partidas[idp];
+		}
+		callback(partida);
+	}
+	this.agregarUsuario=function(nombre,callback){
+		if (!this.usuarios[nombre]){
+			console.log("Nuevo usuario: "+nombre);
+			this.usuarios[nombre]=new Usuario(nombre);
+		}
+		callback(this.usuarios[nombre]);
+	}
+	this.obtenerUsuarios=function(callback){
+		callback(this.usuarios);
+	}
+	this.obtenerPartidas=function(callback){
+		callback(this.partidas);
+	}
+	this.unirAPartida=function(nombre,nick){
+		var partida={};
+		if (this.partidas[nombre] && this.usuarios[nick]){
+			this.partidas[nombre].agregarJugador(this.usuarios[nick]);
+			partida=this.partidas[nombre];
+		}
+		return partida;
+	}
+	this.salir=function(nombrePartida,nick){
+		this.partidas[nombrePartida].salir(nick);
+		if (this.comprobarJugadores(nombrePartida)==0){
+			this.eliminarPartida(nombrePartida);
+		}
+	}
+	this.comprobarJugadores=function(nombrePartida){
+		return Object.keys(this.partidas[nombrePartida].jugadores).length;
+	}
+	this.eliminarPartida=function(nombrePartida){
+		delete this.partidas[nombrePartida];
+	}
+	this.obtenerJugadoresPartida=function(nombrePartida,callback){
+		var jugadores={};
+		if (this.partidas[nombrePartida]){
+			jugadores=this.partidas[nombrePartida].obtenerJugadores();
+		}
+		callback(jugadores);
+	}
+}
+
+function Partida(nombre,idp){
+	this.nombre=nombre;
+	this.idp=idp;
+	this.jugadores={};
+	this.fase=new Inicial();
+	this.agregarJugador=function(usr){
+		this.fase.agregarJugador(usr,this);
+	}
+	this.puedeAgregarJugador=function(usr){
+		this.jugadores[usr.nick]=usr;
+	}
+	this.obtenerJugadores=function(){
+		return this.jugadores;
+	}
+	this.salir=function(nick){
+		delete this.jugadores[nick];
+	}
+}
+
+function Inicial(){
+	this.nombre="inicial";
+	this.agregarJugador=function(usr,partida){
+		partida.puedeAgregarJugador(usr);
+	}
+}
+
+function Jugando(){
+	this.nombre="jugando";
+	this.agregarJugador=function(usr,partida){
+		console.log("El juego ya ha comenzado");
+	}
+}
+
+function Final(){
+	this.nombre="final";
+	this.agregarJugador=function(usr,partida){
+		console.log("El juego ya ha terminado");
+	}
+}
+
+function Usuario(nick){
+	this.nick=nick;
+}
+
+module.exports.Juego=Juego;
